38dc06466f8f5ddad7b8b1a8abd3b59647d1d22a
*yrnzak* <zakkafeero21@icloud.com>
2024-04-21 23:23:31 -0400

    changes

---
+++ app/src/main/java/com/ssaapp/c_eats/MilkAndHoney.kt
@@ -0,0 +1,11 @@
+package com.ssaapp.c_eats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MilkAndHoney : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_milk_and_honey)
+    }
+}
---
+++ app/src/main/java/com/ssaapp/c_eats/StatBucks.kt
@@ -0,0 +1,11 @@
+package com.ssaapp.c_eats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatBucks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stat_bucks)
+    }
+}
