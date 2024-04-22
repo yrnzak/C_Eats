027cb6ecc4ef3a17a9d1fcdac5f401415bfc57e5
*yrnzak* <zakkafeero21@icloud.com>
2024-04-21 23:29:18 -0400

    changes

--- app/src/main/java/com/ssaapp/c_eats/MilkAndHoneyMenuActivity.kt
+++ app/src/main/java/com/ssaapp/c_eats/MilkAndHoneyMenuActivity.kt
@@ -3,9 +3,9 @@ package com.ssaapp.c_eats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class StatBucks : AppCompatActivity() {
+class MilkAndHoneyMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_stat_bucks)
+        setContentView(R.layout.activity_milk_and_honey_menu)
     }
 }
--- app/src/main/java/com/ssaapp/c_eats/StarBucksMenuActivity.kt
+++ app/src/main/java/com/ssaapp/c_eats/StarBucksMenuActivity.kt
@@ -3,9 +3,9 @@ package com.ssaapp.c_eats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MilkAndHoney : AppCompatActivity() {
+class StarBucksMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_milk_and_honey)
+        setContentView(R.layout.activity_star_bucks_menu)
     }
 }
